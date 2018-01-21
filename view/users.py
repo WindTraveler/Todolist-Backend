f0391a79c026ba358909fa19446146f0e230327c
WindTraveler <13301156@bjtu.edu.cn>
2018-01-21 20:31:22 +0800

    跨域问题options

--- view/users.py
+++ view/users.py
@@ -85,9 +85,10 @@ class LoginAPI(MethodView):
             response.headers["Content-Type"] = "application/json; charset=utf-8"
             return response
 
-    def option(self):
+    def options(self):
         response = make_response()
         response.headers["Access-Control-Allow-Origin"] = "*"
+        #response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, X-ID, X-TOKEN, X-ANY-YOUR-CUSTOM-HEADER"
         response.headers["Access-Control-Allow-Methods"] = "GET POST PUT"
         return response
 
